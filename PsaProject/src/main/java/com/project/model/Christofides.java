5e7843827524dc2e885afdbbb5bc9f542728c34c
Rajat Sharma <sharma.raja@northeastern.edu>
2023-03-30 13:08:14 -0400

    Add Christofides findOddDegreeVertices

---
+++ PsaProject/src/main/java/com/project/model/Christofides.java
@@ -0,0 +1,31 @@
+package com.project.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Christofides {
+
+    public static List<Edge> findMST(Graph graph) {
+        return graph.kruskalMST();
+    }
+
+    public static List<Node> findOddDegreeVertices(Graph graph, List<Edge> mst) {
+        Map<Node, Integer> degrees = new HashMap<>();
+        for (Node node : graph.nodes) {
+            degrees.put(node, 0);
+        }
+        for (Edge edge : mst) {
+            degrees.put(edge.source, degrees.get(edge.source) + 1);
+            degrees.put(edge.destination, degrees.get(edge.destination) + 1);
+        }
+        List<Node> oddDegreeNodes = new ArrayList<>();
+        for (Map.Entry<Node, Integer> entry : degrees.entrySet()) {
+            if (entry.getValue() % 2 != 0) {
+                oddDegreeNodes.add(entry.getKey());
+            }
+        }
+        return oddDegreeNodes;
+    }
+}
